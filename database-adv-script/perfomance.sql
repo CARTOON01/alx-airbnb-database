4863e59c4bf88fb157c18885b70424ac0546c979
Cartoon <brianngugi224@gmail.com>
2025-05-16 03:29:09 +0300

    add optimization

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,49 @@
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.first_name AS user_first_name,
+    u.last_name AS user_last_name,
+    p.name AS property_name,
+    p.description AS property_description,
+    pay.amount AS payment_amount,
+    pay.payment_method
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.user_id
+JOIN 
+    properties p ON b.property_id = p.property_id
+LEFT JOIN 
+    payments pay ON b.booking_id = pay.booking_id
+WITH BookingDetails AS (
+    SELECT 
+        b.booking_id,
+        b.user_id,
+        b.property_id,
+        b.start_date,
+        b.end_date,
+        b.total_price
+    FROM 
+        bookings b
+SELECT 
+    bd.booking_id,
+    bd.start_date,
+    bd.end_date,
+    bd.total_price,
+    u.first_name AS user_first_name,
+    u.last_name AS user_last_name,
+    p.name AS property_name,
+    p.description AS property_description,
+    pay.amount AS payment_amount,
+    pay.payment_method
+FROM 
+    BookingDetails bd
+JOIN 
+    users u ON bd.user_id = u.user_id
+JOIN 
+    properties p ON bd.property_id = p.property_id
+LEFT JOIN 
+    payments pay ON bd.booking_id = pay.booking_id
+EXPLAIN ANALYZE SELECT * FROM bookings;
