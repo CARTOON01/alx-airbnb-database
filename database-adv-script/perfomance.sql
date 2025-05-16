a48b200eaf473d4ae777adb17c9b802aef1cd545
Cartoon <brianngugi224@gmail.com>
2025-05-16 11:36:47 +0300

    include where filter

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -16,7 +16,10 @@ JOIN
 JOIN 
     properties p ON b.property_id = p.property_id
 LEFT JOIN 
-    payments pay ON b.booking_id = pay.booking_id;
+    payments pay ON b.booking_id = pay.booking_id
+WHERE 
+    b.start_date >= '2024-01-01'
+    AND b.total_price > 100;
 
 WITH BookingDetails AS (
     SELECT 
@@ -28,6 +31,9 @@ WITH BookingDetails AS (
         b.total_price
     FROM 
         bookings b
+    WHERE 
+        b.start_date >= '2024-01-01'
+        AND b.total_price > 100
 )
 SELECT 
     bd.booking_id,
@@ -49,5 +55,26 @@ JOIN
 LEFT JOIN 
     payments pay ON bd.booking_id = pay.booking_id;
 
-
-EXPLAIN ANALYZE SELECT * FROM bookings;
+EXPLAIN ANALYZE
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.first_name AS user_first_name,
+    u.last_name AS user_last_name,
+    p.name AS property_name,
+    p.description AS property_description,
+    pay.amount AS payment_amount,
+    pay.payment_method
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.user_id
+JOIN 
+    properties p ON b.property_id = p.property_id
+LEFT JOIN 
+    payments pay ON b.booking_id = pay.booking_id
+WHERE 
+    b.start_date >= '2024-01-01'
+    AND b.total_price > 100;
